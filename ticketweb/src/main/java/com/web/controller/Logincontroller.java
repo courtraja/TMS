12a7e2a33abcbcd91d2bf42fcae72940e1b4058e
courtraja <RCS113@india.multivision.net>
2017-02-07 01:04:27 -0500

    updated

---
+++ ticketweb/src/main/java/com/web/controller/Logincontroller.java
@@ -0,0 +1,15 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/")
+public class Logincontroller {
+
+	@GetMapping
+	public String home(){
+		System.out.println("login Page");
+		return "Login.jsp";
+	}
+}
